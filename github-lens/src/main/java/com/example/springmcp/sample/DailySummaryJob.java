f41b06e75f7f0e8c9ba73f37c0bf9c4f4da73d13
vudayani <vudayani@vmware.com>
2025-02-02 16:30:04 +0530

    enhance daily summary prompt

--- github-lens/src/main/java/com/example/springmcp/sample/DailySummaryJob.java
+++ github-lens/src/main/java/com/example/springmcp/sample/DailySummaryJob.java
@@ -3,7 +3,6 @@ package com.example.springmcp.sample;
 import java.util.Map;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -19,29 +18,17 @@ public class DailySummaryJob {
 	@Value("classpath:/prompts/dailySummaryPrompt.st")
     private Resource dailySummaryPrompt;
 	
-	@Value("classpath:/prompts/actionItemsPrompt.st")
-    private Resource actionItemsPrompt;
-
-	@Scheduled(cron = "0 */1 * * * *")
+	@Scheduled(cron = "0 0 8 * * *")
 	public void fetchAndSendGitSummary() {
 		try {
-			System.out.println(chatClient.
-					prompt().user(u -> u.text(dailySummaryPrompt).params(Map.of("repoName","blogging-platform", "repoOwner", "venkat-vmv")))
-					.advisors(new SimpleLoggerAdvisor()).call().content());
+			String response = chatClient
+					.prompt()
+					.user(u -> u.text(dailySummaryPrompt)
+							.params(Map.of("repoName","blogging-platform", "repoOwner", "venkat-vmv")))
+					.call().content();
+			System.out.println(response);
 		} catch (Exception e) {
-			System.err.println("Error fetching daily github summary: " + e.getMessage());
-			e.printStackTrace();
-		}
-	}
-	
-	@Scheduled(cron = "0 */2 * * * *")
-	public void fetchActionItemsAndSendRemainders() {
-		try {
-			System.out.println(chatClient.
-					prompt().system(s -> s.text(actionItemsPrompt).params(Map.of("repoName","blogging-platform", "repoOwner", "venkat-vmv")))
-					.advisors(new SimpleLoggerAdvisor()).call().content());
-		} catch (Exception e) {
-			System.err.println("Error fetching github action items: " + e.getMessage());
+			System.err.println("Error fetching GitHub Summary for repository: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
